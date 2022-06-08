5ef3dcf27aebf1eb6940b51e45f93a3e72ff7a61
mfkldev <263542>
2022-06-08 18:22:16 -0300

    [MFKL-03] refactor - improviment pic-pay-service interface

--- app/src/main/java/com/picpay/desafio/android/domain/api/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/api/PicPayService.kt
@@ -1,11 +1,25 @@
 package com.picpay.desafio.android.domain.api
 
 import com.picpay.desafio.android.domain.model.User
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface PicPayService {
 
     @GET("users")
     fun getUsers(): Call<List<User>>
+
+    companion object {
+        private const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+
+        fun create(): PicPayService = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PicPayService::class.java)
+    }
 }
--- app/src/main/java/com/picpay/desafio/android/presentation/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.core.extensions.gone
 import com.picpay.desafio.android.core.extensions.visible
@@ -14,37 +12,17 @@ import com.picpay.desafio.android.databinding.ActivityMainBinding
 import com.picpay.desafio.android.domain.api.PicPayService
 import com.picpay.desafio.android.domain.model.User
 import com.picpay.desafio.android.presentation.adapter.UserListAdapter
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var mainAdapter: UserListAdapter
 
-    private val url = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
-
-    private val gson: Gson by lazy { GsonBuilder().create() }
-
-    private val okHttp: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            .build()
-    }
-
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(url)
-            .client(okHttp)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-    }
-
     private val service: PicPayService by lazy {
-        retrofit.create(PicPayService::class.java)
+        PicPayService.create()
     }
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
